129c26cc2811ff0fe14a4e201122d83f42998e8f
AhlameMn4 <Ahlammandour490@gmail.com>
2026-05-22 22:53:21 +0100

    Auto create admin

--- build.sh
+++ build.sh
@@ -3,3 +3,21 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "
+from accounts.models import CustomUser
+
+if not CustomUser.objects.filter(username='admin').exists():
+    CustomUser.objects.create_superuser(
+        username='admin',
+        email='admin@ocp.ma',
+        password='Admin@OCP2025',
+        first_name='Administrateur',
+        last_name='RH',
+        role='admin',
+        actif=True,
+        must_change_password=False
+    )
+    print('Admin créé')
+else:
+    print('Admin existe déjà')
+"
